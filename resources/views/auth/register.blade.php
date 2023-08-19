eb0efe359449074218f543610e1a8d9e79cd45c5
asifulmamun <asifulmamun@gmail.com>
2023-08-19 19:46:57 +0600

    in registered form mobile number added and db saved success

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,6 +15,11 @@
             </div>
 
             <div class="mt-4">
+                <x-jet-label for="phone_number" value="{{ __('Phone Number') }}" />
+                <x-jet-input id="phone_number" class="block mt-1 w-full" type="text" name="phone_number" :value="old('phone_number')" required autofocus autocomplete="phone_number" />
+            </div>
+
+            <div class="mt-4">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
