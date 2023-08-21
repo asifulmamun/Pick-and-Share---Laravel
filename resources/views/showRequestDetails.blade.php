57fe36779ffd2222b24135e3081fe7dd08e78ea4
asifulmamun <asifulmamun@gmail.com>
2023-08-21 22:19:17 +0600

    show dtl menu added for apply different user different system

--- resources/views/showRequestDetails.blade.php
+++ resources/views/showRequestDetails.blade.php
@@ -66,30 +66,45 @@
 
 
 {{-- Apply JOB/BID --}}
+
 @if ($driver->status ?? '')
 
     {{-- DRIVER --}}
-    @if ($driver->status == '2')
-    <div class="bg-white py-6 sm:py-8 lg:py-12">
-        <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
-            <div class="flex flex-col items-center justify-between gap-4 rounded-lg bg-gray-100 p-4 sm:flex-row md:p-8">
-                <div>
-                    <h2 class="text-xl font-bold text-indigo-500 md:text-2xl">HELLO, {{ $driver->name; }}! You can apply for this contract.</h2>
-                    <p class="text-gray-600">If the requester interest then ther requester will make a contract with you.
-                        You will be found this in your Dashboard. </p>
-                </div>
+    @if ($bookingRequest->user_id == auth()->user()->id )
+        <div class="bg-white py-6 sm:py-8 lg:py-12">
+            <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+                <div class="flex flex-col items-center justify-between gap-4 rounded-lg bg-gray-100 p-4 sm:flex-row md:p-8">
+                    <div>
+                        <h2 class="text-xl font-bold text-indigo-500 md:text-2xl">HELLO, {{ $driver->name; }}! You can not apply for this contract.</h2>
+                        <p class="text-gray-600">This is your request, you can see details. </p>
+                    </div>
 
-                <a href="#"
-                    class="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">Start
-                    now</a>
+                    <a href="#"
+                        class="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">SEE DETAILS</a>
+                </div>
             </div>
         </div>
-    </div>
+    @elseif ($driver->status == '2')
+        <div class="bg-white py-6 sm:py-8 lg:py-12">
+            <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+                <div class="flex flex-col items-center justify-between gap-4 rounded-lg bg-gray-100 p-4 sm:flex-row md:p-8">
+                    <div>
+                        <h2 class="text-xl font-bold text-indigo-500 md:text-2xl">HELLO, {{ $driver->name; }}! You can apply for this contract.</h2>
+                        <p class="text-gray-600">If the requester interest then ther requester will make a contract with you.
+                            You will be found this in your Dashboard. </p>
+                    </div>
+
+                    <a href="#"
+                        class="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">Start
+                        now</a>
+                </div>
+            </div>
+        </div>
     @endif
     {{-- /DRIVER --}}
 
 @else
-    {{-- GUEST/LOGGED USER --}}
+{{-- GUEST/LOGGED USER --}}
     <div class="bg-white py-6 sm:py-8 lg:py-12">
         <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
             <div class="flex flex-col items-center justify-between gap-4 rounded-lg bg-gray-100 p-4 sm:flex-row md:p-8">
@@ -105,7 +120,6 @@
         </div>
     </div>
     {{-- /GUEST/LOGGED USER --}}
-
 @endif
 {{-- Apply JOB/BID --}}
 
