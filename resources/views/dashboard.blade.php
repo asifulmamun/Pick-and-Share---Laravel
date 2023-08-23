3e4c290f793411d0aceba3633b8d4c38dec4c977
asifulmamun <asifulmamun@gmail.com>
2023-08-23 12:13:01 +0600

    dashboard request linkup for desc

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -130,7 +130,7 @@
                             {{ $bookingRequest->destination }}</h1>
                         <p class="leading-relaxed mb-3">{{ $bookingRequest->journeyDetails}}</p>
                         <div class="flex items-center flex-wrap ">
-                            <a class="text-yellow-500 inline-flex items-center md:mb-2 lg:mb-0">Show Response
+                            <a href="{{ route('showBookingRequestDetails', ['id' => $bookingRequest->id])}}" class="text-yellow-500 inline-flex items-center md:mb-2 lg:mb-0">Show Response
                                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
                                     fill="none" stroke-linecap="round" stroke-linejoin="round">
                                     <path d="M5 12h14"></path>
